fbe0c2c72224f4c489baf247622956ce3289e77d
Simon Kennedy <sffjunkie+code@gmail.com>
2021-06-29 21:54:08 +0100

    WIP

--- laptop/build.sh
+++ laptop/build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 ansible-playbook main.yaml \
+    -e @../vars/secrets.yaml \
     -e configfiles_path=$(realpath ../configfiles/output) \
     -e scriptfiles_path=$(realpath ../scriptfiles) \
-    --ssh-ask-pass --become-ask-pass
+    -u sdk --ask-become-pass

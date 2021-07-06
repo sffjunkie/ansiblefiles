c434fe42095fce46c1125847bdff6e202880606b
Simon Kennedy <sffjunkie+code@gmail.com>
2021-07-06 14:15:54 +0100

    WIP

--- configfiles/build.sh
+++ configfiles/build.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 [ -f requirements.yaml ] && ansible-galaxy install -r requirements.yaml
-ansible-playbook main.yaml
+ansible-playbook build.yaml \
+    -e @../vars/secrets.yaml \
+    "$@"
+    # -e scriptfiles_path=$(realpath ../scriptfiles) \
+    # --ask-vault-pass
--- laptop/configure.sh
+++ laptop/configure.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-ansible-playbook main.yaml \
+ansible-playbook configure.yaml \
     -e @../vars/secrets.yaml \
     -e configfiles_path=$(realpath ../configfiles/output) \
-    -e scriptfiles_path=$(realpath ../scriptfiles)
+    -e scriptfiles_path=$(realpath ../scriptfiles) "$@"
     # -e scriptfiles_path=$(realpath ../scriptfiles) \
     # --ask-vault-pass
